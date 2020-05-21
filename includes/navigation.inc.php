715b86b9bb47976f2373abaae082e5bf34a736b7
williammakassy <makassyw05@gmail.com>
2020-05-21 13:51:54 +0300

    profile and logout link added by williamcodes

--- includes/navigation.inc.php
+++ includes/navigation.inc.php
@@ -32,11 +32,11 @@
                 <img class="img-profile rounded-circle" src="img/wc.png">
               </a>
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
+                <a class="dropdown-item" href="profile.php">
                   <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                   Profile
                   </a>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
