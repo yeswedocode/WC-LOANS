f0b5f7cde78ca3e9a13c285443d447d1f50e9233
williammakassy <makassyw05@gmail.com>
2020-05-21 15:20:04 +0300

    loan personal file

---
+++ addLoan.php
@@ -0,0 +1,69 @@
+<?php include 'includes/header.inc.php'; ?>
+
+<?php include 'includes/sidebar.inc.php'; ?>
+
+<div class="container" style="padding-top:2rem">
+    <div class="row">
+        <div class="col-md-12">
+            <ul class="nav nav-tabs" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#person">Personal Information</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#financial">Financial Information</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#">Additional Information</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col-md-6 mx-auto tab-content" style="padding-top:3rem" id="person">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="text-center text-muted">Step 1:Personal Information</h3>
+                    <hr>
+                    <form action="" method="post">
+                    <label>Image:</label>
+                    <div class="form-group">
+                        <input type="file" name="imgFile" class="form-control">
+                    </div>
+                    <label>Member ID:</label>
+                    <div class="form-group">
+                        <input type="text" name="id" class="form-control" placeholder="Enter Member ID">
+                    </div>
+                    <label>Full Name:</label>
+                    <div class="form-group">
+                        <input type="text" name="uid" class="form-control" placeholder="Enter Full Name">
+                    </div>
+                    <label>Phone:</label>
+                    <div class="form-group">
+                        <input type="text" name="phone" class="form-control" placeholder="Enter Phone Number">
+                    </div>
+                    <label>Address 1:</label>
+                    <div class="form-group">
+                        <input type="text" name="address1" class="form-control" placeholder="Enter Address 1">
+                    </div>
+                    <label>Address 2:</label>
+                    <div class="form-group">
+                        <input type="text" name="address2" class="form-control" placeholder="Enter Address 2">
+                    </div>
+                    <label>Region:</label>
+                    <div class="form-group">
+                        <input type="text" name="region" class="form-control" placeholder="Enter Region">
+                    </div>
+                    <label>City:</label>
+                    <div class="form-group">
+                        <input type="text" name="city" class="form-control" placeholder="Enter City">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="addPersonalDetails" class="form-control btn btn-outline-info" value="Add Personal Info">
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'includes/footer.inc.php'; ?>
