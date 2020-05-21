f9fb3b5c7a751ac3147c86e07350fb46614f6782
williammakassy <makassyw05@gmail.com>
2020-05-21 14:28:39 +0300

    modifed by williamcodes

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -20,8 +20,15 @@
          </div>
      </div>
  </div>
+ <script>
+     $(document).ready(function() {
+         $(".table").prepend($("<thead></thead>").append($(this).find("tr:first"))).dataTable();
+     });
+
+ </script>
  <script src="vendor/jquery/jquery.min.js"></script>
  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="datatables/js/jquery.dataTables.min.js"></script>
  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
  <script src="js/sb-admin-2.min.js"></script>
  <script src="vendor/chart.js/Chart.min.js"></script>
