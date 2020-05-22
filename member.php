1ddf36f01dc92d51606e0efa2f26076c25b187c9
williammakassy <makassyw05@gmail.com>
2020-05-22 09:22:16 +0300

    modified

--- member.php
+++ member.php
@@ -54,26 +54,32 @@
 
         <div class="container">
             <div class="row">
-                <table class="table table-striped" id="dataTable">
-                    <thead class="thead-dark">
-                        <th>Member ID</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Action</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>MEM 100</td>
-                            <td>John Doe</td>
-                            <td>0768693933</td>
-                            <td>
-                                <a href="editMember.php"><span class="badge badge-pill badge-info">Edit</span></a>
-                                |
-                                <a href="editMember.php"><span class="badge badge-pill badge-danger">Delete</span></a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="col">
+                    <a href="addMember.php" class="btn btn-outline-info float-right">Add New Memmber</a>
+                    <br>
+                    <br>
+                    <table class="table table-striped" id="dataTable">
+                        <thead class="thead-dark">
+                            <th>Member ID</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Action</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>MEM 100</td>
+                                <td>John Doe</td>
+                                <td>0768693933</td>
+                                <td>
+                                    <a href="editMember.php"><span class="badge badge-pill badge-info">Edit</span></a>
+                                    |
+                                    <a href="editMember.php"><span class="badge badge-pill badge-danger">Delete</span></a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
         </div>
 
