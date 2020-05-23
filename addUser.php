4436a38c404136d765498a659fb1679f802b2fa2
williammakassy <makassyw05@gmail.com>
2020-05-23 18:13:37 +0300

    modified by williamcodes

--- addUser.php
+++ addUser.php
@@ -92,15 +92,102 @@
                         </div>
                         <label>Password:</label>
                         <div class="form-group">
-                            <input type="password" name="password" class="form-control" placeholder="Enter Password" required>
+                            <input type="password" id="trial" name="password" class="form-control" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" placeholder="Enter Password" required>
                         </div>
                         <label>Repeat Password:</label>
                         <div class="form-group">
-                            <input type="password" name="repeat_password" class="form-control" placeholder="Repeat Password" required>
+                            <input type="password" id="trial" name="repeat_password" class="form-control" placeholder="Repeat Password" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="checkbox" onclick="myFunction()">Show Password
                         </div>
                         <div class="form-group">
                             <input type="submit" role="button" name="addUser" class="form-control btn btn-outline-info" value="Add User">
                         </div>
+
+                        <div class="row">
+                            <div class="coL mx-auto">
+                                <div id="message">
+                                    <h5 class="text-muted">Your Password must contain the following:</h5>
+                                    <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+                                    <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+                                    <p id="number" class="invalid">A <b>number</b></p>
+                                    <p id="length" class="invalid">Minimum <b>8 characters</b></p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <script>
+                             function myFunction()
+                            {
+                                var x = document.getElementById("trial");
+                                if(x.type === "password")
+                                    {
+                                        x.type = "text";
+                                    }
+                                else
+                                    {
+                                        x.type = "password";
+                                    }
+                            }
+                            var myInput = document.getElementById("trial");
+                            var letter = document.getElementById("letter");
+                            var capital = document.getElementById("capital");
+                            var number = document.getElementById("number");
+                            var length = document.getElementById("length");
+
+                            // When the user clicks on the password field, show the message box
+                            myInput.onfocus = function() {
+                              document.getElementById("message").style.display = "block";
+                            }
+
+                            // When the user clicks outside of the password field, hide the message box
+                            myInput.onblur = function() {
+                              document.getElementById("message").style.display = "none";
+                            }
+
+                            // When the user starts to type something inside the password field
+                            myInput.onkeyup = function() {
+                              // Validate lowercase letters
+                              var lowerCaseLetters = /[a-z]/g;
+                              if(myInput.value.match(lowerCaseLetters)) {
+                                letter.classList.remove("invalid");
+                                letter.classList.add("valid");
+                              } else {
+                                letter.classList.remove("valid");
+                                letter.classList.add("invalid");
+                              }
+
+                              // Validate capital letters
+                              var upperCaseLetters = /[A-Z]/g;
+                              if(myInput.value.match(upperCaseLetters)) {
+                                capital.classList.remove("invalid");
+                                capital.classList.add("valid");
+                              } else {
+                                capital.classList.remove("valid");
+                                capital.classList.add("invalid");
+                              }
+
+                              // Validate numbers
+                              var numbers = /[0-9]/g;
+                              if(myInput.value.match(numbers)) {
+                                number.classList.remove("invalid");
+                                number.classList.add("valid");
+                              } else {
+                                number.classList.remove("valid");
+                                number.classList.add("invalid");
+                              }
+
+                              // Validate length
+                              if(myInput.value.length >= 8) {
+                                length.classList.remove("invalid");
+                                length.classList.add("valid");
+                              } else {
+                                length.classList.remove("valid");
+                                length.classList.add("invalid");
+                              }
+                            }
+                        </script>
                     </form>
                 </div>
             </div>
